b99e52e3d994e220ca657ce0c4d571fc5d8b353b
Jeky <jeky.cui@gmail.com>
2014-09-29 15:28:52 -0400

    add plot script

--- plot.py
+++ plot.py
@@ -1,7 +1,14 @@
 from data import *
 
 # OUTPUT DATA FORMAT:
-# JP,TWITTER_ID(AFTER_COMPRESSED),RANK(STARTS FROM 0),SAMPLE_SIZE,VAR1,VAR2,BIAS1,BIAS2
+# 0 JP
+# 1 TWITTER_ID(AFTER_COMPRESSED)
+# 2 RANK(STARTS FROM 0)
+# 3 SAMPLE_SIZE
+# 4 VAR1
+# 5 VAR2
+# 6 BIAS1
+# 7 BIAS2
 
 def loadOutput():
     f = open(OUTPUT_FILE)
@@ -23,4 +30,31 @@ def select(output, conditionList, indexList):
             if not c(d):
                 selected = False
 
-        
+        if selected:
+            r.append([d[i] for i in indexList])
+
+    return r
+
+
+def printPlotScript(result, outputFile, xlabel, ylabel):
+    fout = open(outputFile, 'w')
+    fout.write('data=[%s];\n' % ';'.join([','.join(str(r)) for r in result]))
+    fout.write('plot(data(:,1), data(:,2));\n')
+    fout.write('xlabel(%s);\n' % xlabel)
+    fout.write('ylable(%s);\n' % ylabel)
+
+    fout.close()
+
+
+if __name__ == '__main__':
+    output = loadOutput()
+    print 'Load Output Data:', len(output), 'rows...'
+    for size in [10 ** 3, 10 ** 4, 10 ** 5]:
+        for rank in range(20):
+            result = select(output, [lambda c : c[3] == size, lambda c : c[2] == rank], [0, 4])
+            printPlotScript(result, '/Volumes/Time Machine/jeky/twitter/rw_result/jp-var.m', 'JP', 'VAR')
+            # result = select(output, [lambda c : c[3] == size, lambda c : c[2] == rank], [0, 5])
+            # result = select(output, [lambda c : c[3] == size, lambda c : c[2] == rank], [0, 6])
+
+
+
